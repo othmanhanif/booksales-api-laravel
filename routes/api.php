70e49785f35d8de88d2c35aafc9f55a2049fc211
othman hanif <othmanhanifw06@gmail.com>
2025-06-19 02:22:12 +0700

    Add AuthorController and /api/authors endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/books', [BookController::class, 'index']);
+Route::get('/authors', [AuthorController::class, 'index']);
